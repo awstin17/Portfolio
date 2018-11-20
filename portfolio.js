52baa11c5edfd75a88670948752bae127c1f4c6a
awstin17 <austinwiese.acw@gmail.com>
2018-11-19 21:33:30 -0800

    First project reboot expand section done

--- portfolio.js
+++ portfolio.js
@@ -3,17 +3,30 @@ let snapzExpandSection = document.getElementById('snapzExpand');
 let stockathonExpandSection = document.getElementById('stockathonExpand');
 let bankExpandSection = document.getElementById('bankExpand');
 let header;
+let hovers = document.getElementsByClassName('learn-more');
+console.log(hovers[0])
 
-function expand(project) {
+function expand(project, numberInList) {
 
     if(project.style.height === "0px") {
         project.style.height = "auto";
         header = project.querySelector('.header');
         header.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        changeHoverText(numberInList);
     }
    
     else {
         project.style.height = "0px";
-        arrow.style.display = "none";
+        changeHoverText(numberInList);
     }
+
+function changeHoverText(numberInList) {
+        if(hovers[numberInList].innerHTML === "Learn More") {
+            hovers[numberInList].innerHTML = "Collapse"; 
+        }
+        else {
+            hovers[numberInList].innerHTML = "Learn More";
+        }
+
+}
 }
