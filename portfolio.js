195a96de4701ff0f3d7fc810d7a6ec4caf8858d9
awstin17 <austinwiese.acw@gmail.com>
2019-02-06 12:00:04 -0800

    Added carousel for all projects

--- portfolio.js
+++ portfolio.js
@@ -7,13 +7,13 @@ let hovers = document.getElementsByClassName('learn-more');
 
 function expand(project, numberInList) {
 
-    if(project.className === "unexpanded") {
+    if (project.className === "unexpanded") {
         project.className = "expanded";
         header = project.querySelector('.header');
-        // header.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        header.scrollIntoView({ behavior: 'smooth', block: 'center' });
         changeHoverText(numberInList);
     }
-   
+
     else {
         project.className = "unexpanded";
         changeHoverText(numberInList);
@@ -21,11 +21,47 @@ function expand(project, numberInList) {
 }
 
 function changeHoverText(numberInList) {
-    if(hovers[numberInList].innerHTML === "Learn More") {
-        hovers[numberInList].innerHTML = "Collapse"; 
+    if (hovers[numberInList].innerHTML === "Learn More") {
+        hovers[numberInList].innerHTML = "Collapse";
     }
     else {
         hovers[numberInList].innerHTML = "Learn More";
     }
 
+}
+
+function nextImage(carousel) {
+    el = document.querySelector('#' + carousel);
+
+    if (el.querySelectorAll(".displayingImage")[0].nextElementSibling) {
+        el.querySelectorAll(".displayingImage")[0].nextElementSibling.className = "displayingImage";
+        el.querySelectorAll(".displayingImage")[0].classList.add("hiddenImage");
+        el.querySelectorAll(".displayingImage")[0].classList.remove("displayingImage");
+    }
+
+    else {
+        el.querySelectorAll(".displayingImage")[0].parentElement.firstElementChild.classList.add("displayingImage");
+        el.querySelectorAll(".displayingImage")[0].parentElement.firstElementChild.classList.remove("hiddenImage");
+        el.querySelectorAll(".displayingImage")[1].classList.add("hiddenImage");
+        el.querySelectorAll(".displayingImage")[1].classList.remove("displayingImage");
+    }
+}
+
+function previousImage(carousel) {
+
+    el = document.querySelector('#' + carousel);
+
+    if (el.querySelectorAll(".displayingImage")[0].previousElementSibling) {
+        el.querySelectorAll(".displayingImage")[0].previousElementSibling.classList.remove("hiddenImage");
+        el.querySelectorAll(".displayingImage")[0].previousElementSibling.classList.add("displayingImage");
+        el.querySelectorAll(".displayingImage")[1].classList.add("hiddenImage");
+        el.querySelectorAll(".displayingImage")[1].classList.remove("displayingImage");
+    }
+
+    else {
+        el.querySelectorAll(".displayingImage")[0].parentElement.lastElementChild.classList.add("displayingImage");
+        el.querySelectorAll(".displayingImage")[0].parentElement.lastElementChild.classList.remove("hiddenImage");
+        el.querySelectorAll(".displayingImage")[0].classList.add("hiddenImage");
+        el.querySelectorAll(".displayingImage")[0].classList.remove("displayingImage");
+    }
 }
